70c1467b7ba6a25d67b12b3887f3e7832ff13fa5
987958223 <987958223@qq.com>
2016-03-02 22:56:49 +0800

    v3

--- Lib/Action/Common/AdminAction.class.php
+++ Lib/Action/Common/AdminAction.class.php
@@ -1,8 +1,23 @@
 <?php
 
-class AdminAction extends CommonAction{
-    public function _initialize(){
-        parent::_initialize();
-    }
-}
+class AdminAction extends CommonAction {
 
+    public function _initialize() {
+        parent::_initialize();
+
+        $canVisitActFunc = unserialize(session('actionAndFunction'));
+        
+        if (!isset($canVisitActFunc[MODULE_NAME])) {
+            header("Content-type:text/html;charset=utf-8");
+            echo '无权访问模块 ： ' . MODULE_NAME;
+            exit;
+        } else {
+            if (!in_array(ACTION_NAME,$canVisitActFunc[MODULE_NAME])) {
+                header("Content-type:text/html;charset=utf-8");
+                echo '无权访问方法 ： ' . ACTION_NAME;
+                exit;
+            }
+        }
+    }
+
+}
